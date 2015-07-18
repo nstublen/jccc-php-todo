6227ddee992154c7b8e9e31848d2961544db65a2
Neal Stublen <nstublen@gmail.com>
2015-07-17 23:58:02 -0500

    Handle items.php requests

---
+++ api/items.php
@@ -0,0 +1,17 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] === 'GET') {
+
+}
+
+else if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+}
+
+else if ($_SERVER['REQUEST_METHOD'] === 'PUT') {
+
+}
+
+else if ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
+
+}
